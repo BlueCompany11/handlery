b9748bb51fb3bb64b3116bc69cbc5811a59e2d0d
BlueCompany <gronczewskim@gmail.com>
2017-08-16 12:31:23 +0200

    Add project files.

---
+++ Test handlerow/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test_handlerow
+{
+    public partial class Form1 : Form
+    {
+        MyClass w = new MyClass();
+
+        public Form1()
+        {
+            InitializeComponent();
+            w.ValueChanged += button2_Click;
+            w.someInt = 10;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            w.Fun();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(w.someInt.ToString());
+
+        }
+    }
+}
---
+++ Test handlerow/MyClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_handlerow
+{
+    public class MyClass
+    {
+        public int SomeInt;
+        public int someInt
+        {
+            get
+            {
+                return SomeInt;
+            }
+            set
+            {
+                if (SomeInt != value)
+                {
+                    SomeInt = value;
+                    this.Fun();
+                }
+                else
+                {
+                    someInt = value;
+                }
+
+            }
+        }
+        public event EventHandler ValueChanged;
+        public virtual void OnValueChanged(EventArgs e)
+        {
+            if (ValueChanged != null)
+                ValueChanged(this, e);
+        }
+
+        public void Fun()
+        {
+            ValueChanged.BeginInvoke(null,null,null,null);
+        }
+    }
+}
